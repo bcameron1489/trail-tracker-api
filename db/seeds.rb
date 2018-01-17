7fc2afe688107af8f7ef6780528aefd1898e5c09
bcameron1489 <bcameron1489@gmail.com>
2018-01-17 15:44:21 -0500

    Add seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Trail.create(name: 'Tycoon', mountain: 'Park City', difficulty: 'Intermediate', rating: '7')
+Trail.create(name: 'Pathfinder', mountain: 'Breck', difficulty: 'Intermediate', rating: '6')
+Trail.create(name: 'Turkey Traverse', mountain: 'Big Sky', difficulty: 'Diamond', rating: '9')
+Trail.create(name: 'Detonator', mountain: 'Park City', difficulty: 'Intermediate', rating: '10')
+Trail.create(name: 'Flap Jack', mountain: 'Vail', difficulty: 'Beginner', rating: '7')
+Trail.create(name: 'Detonator', mountain: 'Park City', difficulty: 'Intermediate', rating: '9')

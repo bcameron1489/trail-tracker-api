05d24d57df89a650118782bcbf104fa75d9e6120
bcameron1489 <bcameron1489@gmail.com>
2018-01-18 23:35:37 -0500

    Change restful routes
    Narrow the trails routes down to read only and remove new and edit
    from trackers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :trackers
-  resources :trails
+  resources :trackers, except: %i[new edit]
+  resources :trails, only: %i[index show]
   resources :examples, except: %i[new edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
